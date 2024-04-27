03850c6df0e83978bf567cc81a07658f8139ace0
lybb-web <lybb@uci.edu>
2024-04-26 21:39:48 -0700

    Pixel formation written in functions

--- sketch.js
+++ sketch.js
@@ -1,31 +1,42 @@
-const grid = document.querySelector("#grid");
-let style = getComputedStyle(grid);
-console.log(style);
-console.log(style.width);
-let gridWidth = style.width;
-console.log(gridWidth);
-gridWidth = Number(gridWidth.substring(0, 3));
-console.log(gridWidth);
-let dimension = 16;
-let pixelWidth = gridWidth / dimension;
-
-console.log(pixelWidth);
-
-
-for (let i = 0; i < dimension; i++)
+function getPixelDimensions(grid)
 {
-    let row = document.createElement("div");
-    row.id = "row";
-    row.style.height = String(pixelWidth) + "px";
-    grid.appendChild(row);
-    for (let j = 0; j < dimension; j++)
-    {
-        let pixel = document.createElement("div");
-        pixel.id = "pixel";
-        pixel.style.width = String(pixelWidth) + "px";
-        pixel.style.height = String(pixelWidth) + "px";
-        // pixel.style.backgroundColor = "black";
-        row.appendChild(pixel);
+    let style = getComputedStyle(grid);
+    let gridWidth = style.width;
+    gridWidth = Number(gridWidth.substring(0,3));
+    return gridWidth;
+}
 
+function createPixel(r, h)
+{
+    let pixel = document.createElement("div");
+    pixel.id = "pixel";
+    pixel.style.width = String(h) + "px";
+    pixel.style.height = String(h) + "px";
+    r.appendChild(pixel);
+
+}
+
+function makeGrid(grid, width, dimension)
+{
+    let pixelHeight = width / dimension;
+    for (let i = 0; i < dimension; i++)
+    {
+
+    
+        let row = document.createElement("div");
+        row.id = "row";
+        
+        row.style.height = String(pixelHeight) + "px";
+        for (let j = 0; j < dimension; j++)
+        {
+            createPixel(row, pixelHeight);
+        }
+        grid.appendChild(row);
     }
 }
+
+let dimension = 16;
+let grid = document.querySelector("#grid");
+
+let gridWidth = getPixelDimensions(grid);
+makeGrid(grid, gridWidth, dimension);
